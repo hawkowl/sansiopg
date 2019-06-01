64fbbaad2e94bf880c9a1b00cd580dac5b5a9bee
Amber Brown <hawkowl@atleastfornow.net>
2019-06-02 02:40:31 +1000

    moar parser

---
+++ src/sansiopg/parser.py
@@ -0,0 +1,35 @@
+import attr
+import struct
+
+from .messages import parse_from_buffer
+
+
+@attr.s
+class ParserFeed(object):
+
+    _buffer = attr.ib(default=b"")
+
+    def feed(self, input):
+
+        self._buffer += input
+
+        messages = []
+
+        while True:
+            if len(self._buffer) < 5:
+                # We can't even get the message, so, return
+                return messages
+
+            # Get the length of the message
+            msg_len, = struct.unpack("!i", self._buffer[1:5])
+
+            # Check if we have the whole message
+            if len(self._buffer) < msg_len + 1:
+                print("Dont have for", self._buffer)
+                return messages
+
+            # If we do, split it up
+            msg = self._buffer[0 : msg_len + 1]
+            self._buffer = self._buffer[msg_len + 1 :]
+
+            messages.append(parse_from_buffer(msg))
